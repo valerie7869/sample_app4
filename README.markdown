d24cff37c1d60ac14c5bd9634e7c35abc551a420
valerie <valerie.douglas@openlogic.com>
2014-04-30 15:01:38 -0600

    initial commit- updated README

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial: sample_app4
+
+This is the sample application for sample_app4
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp4::Application.config.secret_token = 'ccfac482dec18f4b6ef0ee41e9d7baef10dae2a1b18613e427326a848aff9ec6af8570ee3ab007b4c8aae25439b160dfaf1677a2698c1dfbef2fb9756cccffc0'
